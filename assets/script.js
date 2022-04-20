9c5355d0d31e9a2c765f93cbadc048ba3a4f4d54
Salma <salmaabdi7494@gmail.com>
2022-04-20 15:00:35 -0500

    fixed minor bug

--- assets/script.js
+++ assets/script.js
@@ -3,63 +3,76 @@
 var displayDad = document.getElementById('displayJokes');
 var dadJokes = document.getElementById('Dad Jokes');
 dadJoke();
-
+dadJokes.addEventListener('click', dadJoke);
 
 async function dadJoke() {
-	var res = await fetch("");
-	var data = await res.json(); //convert to json
-	console.log(data)
-	
-	displayDad.innerText = data.value.joke;
+	var res = await fetch("https://api.icndb.com/jokes/random");
+	var data = await res.json(); //convert to j
+	//console.log(data)
+	if (res.ok){
+		displayDad.innerText = data.value.joke;
+
+	}
 }
 
-dadJokes.addEventListener('click', dadJoke);
 
 
 //RandomJoke
 var displayRandom = document.getElementById('random');
 var randomJoke = document.getElementById('Random Jokes');
 randomJoke();
-
+randomJoke.addEventListener('click', randomJoke);
 
 async function randomJoke() {
 	var res = await fetch("");
 	var data = await res.json();
-	
-	displayRandom.innerText = data.value.joke;
+	//console.log(data)
+
+	if(res.ok){
+
+		displayRandom.innerText = data.value.joke;
+	}
+
 }
 
-randomJoke.addEventListener('click', randomJoke);
+
 
 //Inspirational Quotes
 var inspQuotes = document.getElementById('quotes');
 var quotes = document.getElementById('Inspirational quotes');
 Quotes();
-
+quotes.addEventListener('click', Quotes);
 
 async function Quotes() {
 	var res = await fetch("");
 	var data = await res.json();
 	
-	inspQuotes.innerText = data.value.joke;
+	if (res.ok){
+		inspQuotes.innerText = data.value.joke;
+	}
+
 }
 
-quotes.addEventListener('click', Quotes);
+
+
+
 
 //Sunrise Sunset
 var sunRise = document.getElementById('quotes');
 var sunQuotes = document.getElementById('Inspirational quotes');
 sunRiseQuotes();
-
+sunQuotes.addEventListener('click', sunRiseQuotes);
 
 async function sunRiseQuotes() {
 	var res = await fetch("");
 	var data = await res.json();
+
+	if (res.ok){
 	
 	sunRise.innerText = data.value.joke;
-}
+	}
 
-sunQuotes.addEventListener('click', sunRiseQuotes);
+}
 
 
 
